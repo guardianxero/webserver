2352ef9adbd71216e819ebf056e269074ee62c46
guardianxero <guardianxero@gmail.com>
2013-09-30 22:35:14 -0700

    Start of Web Server code

---
+++ webserver.py
@@ -0,0 +1,18 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+PORT_NUMBER = 8080
+
+class mainHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write("Hello World!")
+		return
+
+try:
+	server=HTTPServer(("",PORT_NUMBER),mainHandler)
+	print 'Started httpserver on port',PORT_NUMBER
+	server.serve_forever()
+except KeyboardInterrupt:
+	print '^C recieved, shutting down the web server'
+	server.socket.close()
